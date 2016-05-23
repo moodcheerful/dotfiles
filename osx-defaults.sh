f109bc4d7e59b5173228eef909ec1cf5a0967782
Elena Volokhina <moodcheerful@gmail.com>
2016-05-23 12:21:12 -0400

    added sidebar width setting to bash script to set OSX defaults

--- osx-defaults.sh
+++ osx-defaults.sh
@@ -30,3 +30,6 @@ defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
 # Save screenshots in PNG format (other options: BMP, GIF, JPG, PDF, TIFF)
 defaults write com.apple.screencapture type -string "png"
 
+# Finder sidebar width
+defaults write com.apple.finder FK_SidebarWidth -int 200
+
